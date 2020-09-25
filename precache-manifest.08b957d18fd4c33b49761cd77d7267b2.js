4c3b396d39e9965675cf8221bf99a19569c35303
P m <52131216@acsind.com>
2020-09-25 16:56:47 +0530

    Updates

---
+++ precache-manifest.08b957d18fd4c33b49761cd77d7267b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c381d535b3a26a9e8ea80e55d4b1abb0",
+    "url": "/quizz-app/index.html"
+  },
+  {
+    "revision": "460eb0f54fda77e8d223",
+    "url": "/quizz-app/static/css/main.be679299.chunk.css"
+  },
+  {
+    "revision": "29e0dcf9a76ce6a6103e",
+    "url": "/quizz-app/static/js/2.597fbd9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quizz-app/static/js/2.597fbd9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460eb0f54fda77e8d223",
+    "url": "/quizz-app/static/js/main.f4953ab4.chunk.js"
+  },
+  {
+    "revision": "331d8244aca78368d136",
+    "url": "/quizz-app/static/js/runtime-main.6061b28b.js"
+  }
+]);
